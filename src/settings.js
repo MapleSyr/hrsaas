9e495fdc92a82d8f15107f5a1a6228fe07b59932
maplesyrup <2735331997@qq.com>
2022-01-24 13:31:33 +0800

    人资项目初始化

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    title: 'Vue Admin Template',
+
+    /**
+     * @type {boolean} true | false
+     * @description Whether fix the header
+     */
+    // 头部固定
+    fixedHeader: false,
+
+    /**
+     * @type {boolean} true | false
+     * @description Whether show the logo in sidebar
+     */
+    // 侧边栏固定
+    sidebarLogo: false
+}
